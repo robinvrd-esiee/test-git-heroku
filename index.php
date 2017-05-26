f6ef10f2f40ee36539519854c74f11d6bc1b53b4
robin-verdier <verdier.robin@gmail.com>
2017-05-26 09:35:11 +0000

    commit c9-2

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-
-<?php echo 'ceci est un test de déploiement inter-services.'; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+    </head>
+    <body>
+        <?php 
+        $a = 5;
+        echo 'ceci est un test de déploiement inter-services. Ceci est une variable $a = '.$a; 
+        ?>
+    </body>
+</html>
 
 
