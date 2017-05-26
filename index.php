91b6b245f95604424d68d0296807de49736da5fc
robin-verdier <verdier.robin@gmail.com>
2017-05-26 09:40:59 +0000

    commit c9-3

--- index.php
+++ index.php
@@ -2,11 +2,15 @@
 <html>
     <head>
         <meta charset="UTF-8" />
+        <title>Test Heroku Deployment</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="dist/bootstrap-theme.min.css">
     </head>
     <body>
         <?php 
         $a = 5;
-        echo 'ceci est un test de déploiement inter-services. Ceci est une variable $a = '.$a; 
+        echo 'ceci est un test de déploiement inter-services. Ceci est une variable $a = '.$a.'.'; 
         ?>
     </body>
 </html>
